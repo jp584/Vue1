207163116e9aedca624f4212e4db882f03dc8284
CarlosFULLHD <119177486+CarlosFULLHD@users.noreply.github.com>
2023-04-13 11:45:30 -0400

    yes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>BANCO MERCANTIL SANTA CRUZ</h1>
+
+    <img
+      alt="Vue logo"
+      src="https://is4-ssl.mzstatic.com/image/thumb/Purple126/v4/a6/f6/bb/a6f6bbed-187e-b8b2-4622-36d44c4e8b2b/AppIcon-1x_U007emarketing-0-5-0-85-220.png/256x256bb.jpg"
+    />
+    <HelloWorld
+      msg="Bienvenido Willy, estamos aqui para ayudarte a encontrar empleo"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
