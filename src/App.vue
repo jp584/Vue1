c7f3954c0167033e686f37cd0eef2968a343fb0a
jp584 <79720811+jp584@users.noreply.github.com>
2023-04-13 12:07:14 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,52 @@
       alt="Vue logo"
       src="https://is4-ssl.mzstatic.com/image/thumb/Purple126/v4/a6/f6/bb/a6f6bbed-187e-b8b2-4622-36d44c4e8b2b/AppIcon-1x_U007emarketing-0-5-0-85-220.png/256x256bb.jpg"
     />
+     <!--CARRUSEL INICIO-->
+    <center>
+      <div id="demo" class="carousel slide" data-ride="carousel">
+        <!-- Barra de indicadores para ir a una imagen del Slide -->
+        <ul class="carousel-indicators">
+          <li data-target="#demo" data-slide-to="0" class="active"></li>
+          <li data-target="#demo" data-slide-to="1"></li>
+          <li data-target="#demo" data-slide-to="2"></li>
+        </ul>
+        <!-- Slide de Imagenes -->
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="https://lpz.ucb.edu.bo/wp-content/uploads/2021/10/Mercantil-Santa-Cruz.png"
+              alt="Los Angeles"
+              width="800"
+              height="450"
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://jornada.com.bo/wp-content/uploads/2022/08/El-Banco-Mercantil-Santa-Cruz-lanza-un-nuevo-Seguro-Vehicular-con-pago-en-cuotas-mensuales.jpeg"
+              alt="Chicago"
+              width="800"
+              height="450"
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              src="https://boliviaemprende.com/wp-content/uploads/2015/10/banco-mercantil.jpg"
+              alt="New York"
+              width="800"
+              height="450"
+            />
+          </div>
+        </div>
+        <!-- Controles para desplazar imagenes Izquierda o Derecha -->
+        <a class="carousel-control-prev" href="#demo" data-slide="prev">
+          <span class="carousel-control-prev-icon"></span>
+        </a>
+        <a class="carousel-control-next" href="#demo" data-slide="next">
+          <span class="carousel-control-next-icon"></span>
+        </a>
+      </div>
+    </center>
+    <!--CARRUSEL FIN-->
     <HelloWorld
       msg="Bienvenido Willy, estamos aqui para ayudarte a encontrar empleo"
     />
